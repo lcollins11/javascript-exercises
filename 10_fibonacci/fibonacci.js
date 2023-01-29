fb06d4f87f5b3905c8167a21ac972469f61f4240
Luke Collins <lukenatcollins@gmail.com>
2023-01-29 19:54:07 +1100

    Completed Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number < 0) return "OOPS";
+    if(number == 1 || number == 2){
+        return 1;
+    }
+    let fibSequence = new Array(number+2);
+    let index = 2;
+    fibSequence[0] = 0;
+    fibSequence[1] = 1;
+    for(index; index <=number; index++){
+        fibSequence[index] = fibSequence[index-1] + fibSequence[index-2];
+    }
+    return fibSequence[number];
 };
 
 // Do not edit below this line
