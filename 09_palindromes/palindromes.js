d067d9e43bb11d43fe049741d5cb6988508a134e
Luke Collins <lukenatcollins@gmail.com>
2023-01-29 19:09:14 +1100

    Completed palindrome task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let oldWord = word.toLowerCase()
+                    .replace(/[^\w\s\']|_/g, "")
+                    .replace(/\s/g, "");
+    const reversedWord = Array.from(oldWord).reverse().join('');
+    return oldWord == reversedWord;
 };
 
 // Do not edit below this line
