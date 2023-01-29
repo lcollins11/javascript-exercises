a47b97fa1af9d3c6019eca13b0dda1fd28e19662
Luke Collins <lukenatcollins@gmail.com>
2023-01-29 14:00:19 +1100

    completed some tasks

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length == 0){
+    return 0;
+  }
+	return array.reduce((a, b) => a+b);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a*b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = a;
+	for(let i = 2; i <= b; i++){
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = a;
+  if(a === 0 || a === 1){
+    return 1;
+  }
+  while(a > 1){
+    a--;
+    result *= a;
+  }
+  return result;
 };
 
 // Do not edit below this line
